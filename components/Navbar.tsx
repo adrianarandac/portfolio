ac8a7f3be4ab2cfd35e96a14e9fe856d69f86864
AdrianAranda <adrienaranda@gmail.com>
2022-01-19 14:00:58 +0100

    started styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-<div className="flex items-baseline bg-yellow-300 -skew-y-12">
-        <div className="text-black text-5xl mr-3">ADRIÁN ARANDA</div>
-        <div>
-          <Link href="/about">
-            <a className="text-black">About Me</a>
-          </Link>{" "}
-          <Link href="/projects">
-            <a className="text-black">/ Projects</a>
-          </Link>
-          <Link href="/cv">
-            <a className="text-black"> / CV</a>
-          </Link>
-        </div>
+    <div className="flex items-baseline justify-around py-3">
+      <div className="text-white text-4xl font-vietnam ml-4">
+        <Link href="/">
+          <a> ADRIAN ARANDA</a>
+        </Link>{" "}
       </div>
-  )
-}
+      <div className="w-3/5 flex justify-end gap-10">
+        <Link href="/about">
+          <a className="text-white whitespace-nowrap">About Me</a>
+        </Link>{" "}
+        <Link href="/projects">
+          <a className="text-white ">Projects</a>
+        </Link>
+        <Link href="/cv">
+          <a className="text-white mr-4">CV</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ pages/projects.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const projects = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-white">so many projects wow</h1>
+    </div>
+  );
+};
+
+export default projects;
