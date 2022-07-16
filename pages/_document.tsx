def432aa0da0e3850007243797bbae0b18e24000
Adrián Aranda Cano <adrienaranda@gmail.com>
2022-07-16 22:00:24 +0300

    Fixed more stylings

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html className="w-screen">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@600&display=swap"
