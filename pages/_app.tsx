d52aa9bbd7e951299e4f57354b29355f06946afa
adrian.aranda <adrian.aranda@pleo.io>
2026-06-18 12:07:10 +0200

    Refine KIN mobile layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
 import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isKinPage = router.pathname.startsWith("/kin");
+
   return (
     <ThemeProvider attribute="class" defaultTheme="night" enableSystem={false}>
-      <div className="app-shell">
+      <div className={`app-shell ${isKinPage ? "app-shell--kin" : ""}`}>
         <Component {...pageProps} />
         <Footer />
       </div>
